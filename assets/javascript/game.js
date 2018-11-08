35a2a5acd13a40853aeb4945bd250e0a343a441e
watercolorhorse <29869241+llcook@users.noreply.github.com>
2018-11-07 20:44:57 -0700

    Got losses and guessesLeft counts to update in HTML.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,32 +4,28 @@ var guesses = 10;
 var guessesLeft = 10;
 var guessedLetters = [];
 
-var computerChoices= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-function updateGuessesLeft () {
-    document.querySelector("guessesLeft").innerHTML = "Guesses left: " + guessesLeft;
-};
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 document.onkeyup = function (event) {
-
-    var userOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     
     var userChoice = event.key.toUpperCase();
 
-    // APP ACCEPTS USERCHOICE ONLY IF IT'S AVAILABLE IN USEROPTIONS
-    if (userOptions.indexOf(userChoice) !== -1) {
+    var computerPick = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    // APP ACCEPTS USERCHOICE ONLY IF IT'S IN ALPHABET
+    if (alphabet.indexOf(userChoice) !== -1) {
         console.log(userChoice);
-    }
-
-    var computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-    var userOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-    if (userOptions.indexOf(userChoice) > -1) {
 
         if (userChoice === computerPick) {
             wins++;
         } else {
-            losses++
+            losses++;
+            guessesLeft--;
         }
+
+        document.getElementById("wins").textContent = wins;
+        document.getElementById("losses").textContent = losses;
+        document.getElementById("guessesLeft").textContent = guessesLeft;
+        document.getElementById("guessedLetters").textContent = guessedLetters;
+
     }}
